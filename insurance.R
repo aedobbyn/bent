3fc9945e3d52a582c629c06f9a43282f48277bbe
Amanda Dobbyn <amanda.e.dobbyn@gmail.com>
2021-06-05 00:25:01 -0400

    finish insurance

--- insurance.R
+++ insurance.R
@@ -3,6 +3,8 @@ library(googlesheets4)
 
 source("utils.R")
 
+SS_DEST <- "https://docs.google.com/spreadsheets/d/1VT6emqnMNFeSR46E_buGbTERK3gu-Ja6PYgEVvB0keg/edit#gid=0"
+
 SS_FULL_DOC <- "https://docs.google.com/spreadsheets/d/1vCg_6Y8fFbAsutROBmh5Jvkpd20xMRSWJu7_lVGVV0k/edit#gid=0"
 
 SS_INSURANCE <- "insurance.csv"
@@ -16,7 +18,7 @@ full <-
   full_source %>% 
   clean_full() %>% 
   distinct(first_name, last_name) %>% 
-  mutate_all(tolower)
+  mutate_all(tolower) 
 
 insurance_source <- 
   readr::read_csv(SS_INSURANCE) 
@@ -28,13 +30,36 @@ insurance <-
   transmute(
     first_name, 
     last_name,
-    date_usau_waiver_signed = usau_waiver_2021_waiver_signed,
-    date_disease_waiver_signed = infectious_diseases_waiver_2021_waiver_signed,
+    signed_usau_waiver = !is.na(usau_waiver_2021_waiver_signed),
+    signed_disease_waiver = !is.na(infectious_diseases_waiver_2021_waiver_signed),
     has_membership = !is.na(products)
   )
   
 joined <- 
   full %>% 
-  left_join(insurance)
+  left_join(insurance) %>% 
+  arrange(first_name, last_name) %>% 
+  mutate_all(
+    tidyr::replace_na, 
+    FALSE
+  ) %>% 
+  mutate(
+    good_to_go = 
+      case_when(
+        signed_usau_waiver & signed_disease_waiver & has_membership ~ "yes",
+        TRUE ~ "no"
+      )
+  ) %>% 
+  select(
+    contains("name"),
+    good_to_go,
+    everything()
+  )
 
+names(joined) <- names(joined) %>% snakecase::to_title_case() %>% tolower()
 
+write_sheet(
+  joined,
+  SS_DEST,
+  sheet = "insurance"
+)
